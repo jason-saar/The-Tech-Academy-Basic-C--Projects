8368b386ec99fcb14b0eb38c977cf35d2be84b95
Jason Saar <jason.saar@gmail.com>
2019-11-17 18:30:26 -0800

    Exception Handling

---
+++ ExceptionHandlinmg/ExceptionHandlinmg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        try
+        {
+            List<int> integers = new List<int>() { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };        //List of random integers
+            Console.WriteLine("Enter a number.");
+            float divisor = (float) Convert.ToDouble(Console.ReadLine());                       //Explicitly type cast Double as float and assign to divisor
+            foreach (int dividend in integers)                                                  //Foreach loop through using dividend as index value in list integers
+            {
+                float result = dividend / divisor;                                              //Calculate dividend/divisor and assign answer to result
+                Console.WriteLine(dividend + " / " + divisor + " = " + result);                 //Print equation.
+            }
+        }
+        catch (FormatException)                                                                 //Catch format exception ex. string, throw error.
+        {
+            Console.WriteLine("Please type a whole number.");
+        }
+        catch (DivideByZeroException)                                                           //Catch division by zero, throw error.
+        {
+            Console.WriteLine("You cannot divide by zero.");
+        }
+        finally                                                                                 //End of try/catch block, pause.
+        {
+            Console.ReadLine();
+        }
+        Console.WriteLine("The program has emerged from a try/catch block");                   //Anounce emergence from block.
+        Console.ReadLine();
+
+    }
+}
+
