1c4364c81c6ea12f38eebbca4d03425c2b8fdb71
Jason Saar <jason.saar@gmail.com>
2019-11-23 13:21:07 -0800

    Methods

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Math
+    {
+        public static int Add(int i)
+        {
+            return i + 10;
+        }
+
+        public static int Subtract(int i)
+        {
+            return i - 10;
+        }
+
+        public static int Multiply(int i)
+        {
+            return i * 10;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer:");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Add(i));
+            Console.WriteLine(Math.Subtract(i));
+            Console.WriteLine(Math.Multiply(i));
+            Console.ReadLine();
+        }
+    }
+}
