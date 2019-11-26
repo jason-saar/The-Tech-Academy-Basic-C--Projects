465a66cc2d1afe31fd3af8d78b4cd0f8f6620b7f
Jason Saar <jason.saar@gmail.com>
2019-11-25 20:04:12 -0800

    MoreMethods3

---
+++ MoreMethods3/MoreMethods3/Operator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods3
+{
+    class Operator
+    {
+        public Operator()
+        {
+
+        }
+
+        public double Operate(double x, double y = 2)
+        {
+            x = Math.Pow(x, y);
+            return x;
+        }
+    }
+}
---
+++ MoreMethods3/MoreMethods3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator op = new Operator();
+            double x;
+            string y;
+            Console.WriteLine("Enter the first number: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number (optional): ");
+            y = Console.ReadLine();
+            if (!string.IsNullOrEmpty(y))
+            {
+                Console.WriteLine(op.Operate(x, Convert.ToDouble(y)));
+            } else
+            {
+                Console.WriteLine(op.Operate(x));
+            }
+            Console.ReadLine();
+        }
+    }
+}
