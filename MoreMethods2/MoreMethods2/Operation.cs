037ebf2143cd8819f574188db0813356b314517c
Jason Saar <jason.saar@gmail.com>
2019-11-25 19:27:56 -0800

    MoreMethods2

---
+++ MoreMethods2/MoreMethods2/Operation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    class Operation
+    {
+        public Operation()
+        {
+
+        }
+        public int Operate(int x)
+        {
+            x = ++x;
+            return x;
+        }
+
+        public int Operate(double x)
+        {
+            x = Math.Floor(x);
+            x = --x;
+            return Convert.ToInt32(x);
+        }
+
+        public int Operate(string s)
+        {
+            int x;
+            Int32.TryParse(s, out x);
+            x = x + 2;
+            return x;
+        }
+    }
+}
---
+++ MoreMethods2/MoreMethods2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            Console.WriteLine(operation.Operate(10));
+            Console.WriteLine(operation.Operate(13.2));
+            Console.WriteLine(operation.Operate("5"));
+            Console.ReadLine();
+        }
+    }
+}
