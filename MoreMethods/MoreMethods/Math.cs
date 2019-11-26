e5e61a65054e1fe2c36062180f1c39f57286f5a8
Jason Saar <jason.saar@gmail.com>
2019-11-25 19:08:24 -0800

    MoreMethods

---
+++ MoreMethods/MoreMethods/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Math
+    {
+        public Math()
+        {
+
+        }
+        public void Multiply(int x, int y)
+        {
+            x = x * y;
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ MoreMethods/MoreMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Multiply(5, 6);
+            math.Multiply(x: 10, y: 12);
+            Console.ReadLine();
+        }
+    }
+}
