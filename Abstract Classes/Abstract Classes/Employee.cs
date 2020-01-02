eec0b8b1f0f40b90556638175c193fd95dc1c99b
Jason Saar <jason.saar@gmail.com>
2020-01-01 17:27:28 -0800

    Generics

--- Abstract Classes/Abstract Classes/Employee.cs
+++ Abstract Classes/Abstract Classes/Employee.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Abstract_Classes
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
 
     {
         public int Id { get; set; }
+        public List<T> Things { get; set; }
 
         public override void SayName()
         {
@@ -22,38 +23,38 @@ namespace Abstract_Classes
             Environment.Exit(0);
         }
 
-        public static bool operator== (Employee employee1, Employee employee2)
-        {
-            if (employee1.Id == employee2.Id)
-                return true;
-            else
-                return false;
-        }
+        //public static bool operator== (Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
 
-        public static bool operator!= (Employee employee1, Employee employee2)
-        {
-            if (employee1.Id != employee2.Id)
-                return true;
-            else
-                return false;
-        }
+        //public static bool operator!= (Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id != employee2.Id)
+        //        return true;
+        //    else
+        //        return false;
+        //}
 
-        public override bool Equals(object obj)
-        {
-            Employee employee = obj as Employee;
+        //public override bool Equals(object obj)
+        //{
+        //    Employee employee = obj as Employee;
 
-            return !Object.ReferenceEquals(null, employee)
-                && Int32.Equals(Id, employee.Id);
-        }
+        //    return !Object.ReferenceEquals(null, employee)
+        //        && Int32.Equals(Id, employee.Id);
+        //}
 
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 13;
-                hash = (hash * 7) + (!Object.ReferenceEquals(null, Id) ? Id.GetHashCode() : 0);
-                return hash;
-            }
-        }
+        //public override int GetHashCode()
+        //{
+        //    unchecked
+        //    {
+        //        int hash = 13;
+        //        hash = (hash * 7) + (!Object.ReferenceEquals(null, Id) ? Id.GetHashCode() : 0);
+        //        return hash;
+        //    }
+        //}
     }
 }
--- Abstract Classes/Abstract Classes/Program.cs
+++ Abstract Classes/Abstract Classes/Program.cs
@@ -10,22 +10,46 @@ namespace Abstract_Classes
     {
         static void Main(string[] args)
         {
-            Employee employee1 = new Employee
+            Employee<string> employee1 = new Employee<string>
             {
-                Id = 1234
+                Things = new List<string> { "Thing 1", "Thing 2", "Thing 3", "Thing 4" }
             };
-            Employee employee2 = new Employee
+
+            Employee<int> employee2 = new Employee<int>
             {
-                Id = 1234
+                Things = new List<int> { 1, 2, 3, 4 }
             };
-            if (employee1 == employee2)
-                Console.WriteLine("Employee1 and Employee2 have the same Id.");
-            else
-                Console.WriteLine("Employee1 and Employee2 do not have the same Id.");
+
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
             Console.ReadLine();
+
+            //Employee employee1 = new Employee
+            //{
+            //    Id = 1234
+            //};
+            //Employee employee2 = new Employee
+            //{
+            //    Id = 1234
+            //};
+            //if (employee1 == employee2)
+            //    Console.WriteLine("Employee1 and Employee2 have the same Id.");
+            //else
+            //    Console.WriteLine("Employee1 and Employee2 do not have the same Id.");
+            //Console.ReadLine();
 
             //IQuittable quittable = new Employee();
             //quittable.Quit();
+
+
         }
     }
 }
