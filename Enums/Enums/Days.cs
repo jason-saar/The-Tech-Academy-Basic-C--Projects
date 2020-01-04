ceaa42ee8db7b3412342c8a9bcf1416927ad7a2b
Jason Saar <jason.saar@gmail.com>
2020-01-03 19:29:52 -0800

    Enum

---
+++ Enums/Enums/Days.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Days
+    {
+        public Day Day { get; set; }
+
+        public string UppercaseFirst(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return string.Empty;
+            return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+        }
+        public void Parse(string d)
+        {
+            try
+            {
+                Day day = (Day)Enum.Parse(typeof(Day), d);
+                Console.WriteLine("It is " + day);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+        }
+    }
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = new Days();
+            string day;
+
+            Console.WriteLine("Enter the current day of the week.");
+            day = Console.ReadLine();
+            day = days.UppercaseFirst(day);
+
+            days.Parse(day);
+
+            Console.Read();
+            
+
+
+        } 
+    }
+}
