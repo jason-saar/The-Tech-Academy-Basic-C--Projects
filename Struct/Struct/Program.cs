d4e2f57e6a55edd14fcd2b29d6ee263a0daacfe2
Jason Saar <jason.saar@gmail.com>
2020-01-08 21:59:16 -0800

    Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number
+            {
+                Amount = 3.14M
+            };
+            Console.WriteLine(num.Amount);
+            Console.Read();
+        }
+    }
+}
