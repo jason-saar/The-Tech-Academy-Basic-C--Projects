42ae340271cbc0fa267f97b26b73c7729984cf39
Jason Saar <jason.saar@gmail.com>
2020-01-19 15:54:56 -0800

    DateTime

---
+++ Date Time/Date Time/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine(futureTime);
+            Console.Read();
+        }
+    }
+}
