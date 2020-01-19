37a2f0e01dc8fd5a95b06d7719ae7df00bebe373
Jason Saar <jason.saar@gmail.com>
2020-01-19 14:14:06 -0800

    File Input/Output

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number: ");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Jason\Documents\log.txt", num);                // Write text to .txt file in directory
+            string text = File.ReadAllText(@"C:\Users\Jason\Documents\log.txt");        // Read .txt file and assign all text to the string text
+            Console.WriteLine(text);
+            Console.ReadLine();
+
+        }
+    }
+}
