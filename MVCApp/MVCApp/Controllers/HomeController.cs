b1d64c4178f56da500a3cd9a21da50221222ae35
Jason Saar <jason.saar@gmail.com>
2020-04-11 11:14:08 -0700

    MVC
    Practice apps using MVC

---
+++ MVCApp/MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jason";
+            user.LastName = "Saar";
+            user.Age = 26;
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid Page");
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=0)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+    }
+}
